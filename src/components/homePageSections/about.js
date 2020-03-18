209a06ba0e8323e2b4eec8b913435db619feac5d
Joseph Wyman <jwymandev@gmail.com>
2020-03-18 16:06:27 -0400

    Comma

--- src/components/homePageSections/about.js
+++ src/components/homePageSections/about.js
@@ -11,7 +11,7 @@ function About(props) {
         <h2>Joseph Wyman</h2>
         <h3>Full Stack Web Developer</h3>
         <p>
-          Hello there! I'm Joe a passionate self-taught web developer on a
+          Hello there! I'm Joe, a passionate self-taught web developer on a
           journey to never stop learning. My passion for learning isn't only
           seen in web development but throughout many aspects of my life,
           including foreign language (Spanish), and music (guitar, keyboard).
