7b3a45867da2b855d1200c5cde7231dcbf552fbe
Joseph Wyman <jwymandev@gmail.com>
2020-03-18 10:10:03 -0400

    project photo max width strict px

--- src/projectPages/nodeFitess.js
+++ src/projectPages/nodeFitess.js
@@ -66,6 +66,39 @@ function NodeFitness(props) {
               <li>Challenge/Compete against Friends</li>
               <li>Set Goals</li>
             </ul>
+            <h3>Project Story</h3>
+          <p>
+            This project is an adaptation of another project, the Fitness Challenge App.
+            To expand my knowledge but avoiding coming up with a brand new idea I rewrote the 
+            Challenge Fitness App in completely different technologies.
+              The app was previously written with django, bootstrap/CSS, postgreSQL
+              and vanillaJS, but this version is written with react, node, mongoDB, redis, 
+              django REST framework and css flexbox. Below is the story for 
+              the original Fitness Challenge App
+            </p>
+
+            <p>
+            My family considers themselves occasional workout enthusiasts that tend to 
+            get competitive at any sign of competition. One year, in order to promote 
+            each other to be healthier we started a fitness competition, simply 
+            tracking our sleep, steps, workout and diet by logging it into 
+            google sheets. As soon as I got my programming skills up, I knew exactly 
+            what I wanted to build, a web app that would allow my family to easily track
+            thier health habits and compete against one another by scoring points for how 
+            healthy they had been. The project ended up growing well beyond what was once 
+            just a simple google sheet, allowing users to set goals for themselves, 
+            challenge other family members and see all their hard work laid out in 
+            visually appealing graphs.
+          </p>
+          <h3>Project Challenges</h3>
+          <p>
+            The main challenges I faced was taking the leap to write the program in a test-drive 
+            development manner. This worked out well at first but as the program grew, and my self
+            imposed time limit of two week crept closer, I saw myself aboning strict TDD rules.
+            Although I did not reach the complete code coverage I was seeking I did have to deal with
+            the trade-offs of TDD, better written code, but an uptick in project length.
+           
+          </p>
           </div>
         </div>
       
